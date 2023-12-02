3710ffeba709ef2494e311aca50dcfbb1c8edf5d
Ref <ref-xx@users.noreply.github.com>
2023-12-02 04:45:11 +0300

    initial
    So you can check the code. There is a executable binary in zip file

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ZXAnimPacker128
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(350, 368);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load SCR Images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(12, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Move Up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(93, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Move Down";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Location = new System.Drawing.Point(368, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 368);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(624, 407);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Pack";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(319, 316);
+            this.listBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(577, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Delay, 0=nodelay, 50=1second, max=255:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 442);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "ZX Animation Packer 128 by Arda Erdikmen (ref) for helpcomputer0 2023";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ZXAnimPacker128
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pickfiles();
+        }
+
+        private void Pack()
+        {
+            byte delay = Convert.ToByte(textBox1.Text);
+            int idx = 0;
+            byte[] PackArray = new byte[6912];
+            int offset = 6912;
+            do
+            {
+                int first = idx;
+                int second = idx + 1;
+                if (second > listBox1.Items.Count - 1)
+                {
+                    //reload last frame and compare with first frame to generate a loop.
+                    second = 0;
+                }
+                //listBox2.Items.Add(first.ToString() + " vs. " + second.ToString());
+                byte[] DeltabyteArray = ReadBytesFromFile(listBox1.Items[first].ToString(), 6912);
+                byte[] ComparebyteArray = ReadBytesFromFile(listBox1.Items[second].ToString(), 6912);
+                
+                if (idx == 0)
+                {
+                    Array.Copy(DeltabyteArray, PackArray, 6912);
+                    listBox2.Items.Add("1.Frame, NewBytes:" + (PackArray.Length).ToString());
+                    
+                }
+                List<Difference> differences = FindDifferences(DeltabyteArray, ComparebyteArray);
+
+                int lastidx = 0;
+                for (int x = 0; x < differences.Count; x++)
+                {
+                    if ((lastidx<6144) && (differences[x].Index>=6144))
+                    {
+                        lastidx=6500; //no more skip
+                        PackArray = AddBytes(PackArray, 255, 254, 0); //MARK as attribute section
+                    }
+                    byte[] bytes = BitConverter.GetBytes(differences[x].Index+16384);
+                    PackArray = AddBytes(PackArray, bytes[1], bytes[0], differences[x].Value2);
+                    //listBox2.Items.Add((differences[x].Index + 16384).ToString() + ":" + bytes[1].ToString() + "-" + bytes[0].ToString() + "=" + differences[x].Value2.ToString());
+                }
+                PackArray = AddBytes(PackArray, 255, 255, delay); //mark end of frame
+                listBox2.Items.Add((1 + idx).ToString() + ".Frame, NewBytes:" + (PackArray.Length - offset).ToString());
+                offset = PackArray.Length;
+                idx++;
+            } while (idx<listBox1.Items.Count);
+            PackArray = AddBytes(PackArray, 255, 253, 0); //end of frames.
+
+            listBox2.Items.Add("Total Bytes:" + PackArray.Length.ToString());
+            if (PackArray.Length < 32768) listBox2.Items.Add("Great! it fits in two banks! Still 48K!");
+            string fileName = Path.GetFileNameWithoutExtension(listBox1.Items[0].ToString());
+            File.WriteAllBytes(fileName+".zxa", PackArray);
+            listBox2.Items.Add("File Saved:" + fileName + ".zxa");
+        }
+
+        static byte[] AddBytes(byte[] originalArray, params byte[] bytesToAdd)
+        {
+            // Create a new array with enough space for the existing and new bytes
+            byte[] newArray = new byte[originalArray.Length + bytesToAdd.Length];
+
+            // Copy the existing bytes to the new array
+            Array.Copy(originalArray, newArray, originalArray.Length);
+
+            // Copy the new bytes to the new array
+            Array.Copy(bytesToAdd, 0, newArray, originalArray.Length, bytesToAdd.Length);
+
+            return newArray;
+        }
+
+        private void MoveItem(int direction)
+        {
+            // Check if an item is selected and if it can be moved in the specified direction
+            if (listBox1.SelectedItem != null && listBox1.SelectedIndex + direction >= 0 && listBox1.SelectedIndex + direction < listBox1.Items.Count)
+            {
+                // Swap the items in the ListBox
+                int newIndex = listBox1.SelectedIndex + direction;
+                object selected = listBox1.SelectedItem;
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                listBox1.Items.Insert(newIndex, selected);
+                listBox1.SelectedIndex = newIndex;
+            }
+        }
+
+        private byte[] ReadBytesFromFile(string filePath, int numBytes)
+        {
+            // Read the specified number of bytes from the file
+            byte[] byteArray;
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byteArray = new byte[numBytes];
+                fs.Read(byteArray, 0, numBytes);
+            }
+
+            return byteArray;
+        }
+
+        private void pickfiles()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Allow selecting multiple files
+            openFileDialog.Multiselect = true;
+
+            // Set the title of the dialog
+            openFileDialog.Title = "Select Files";
+
+            // Set the initial directory (optional)
+            //openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Set the file types allowed (optional)
+            openFileDialog.Filter = "ZX Spectrum Screen Files (*.scr)|*.SCR|All Files (*.*)|*.*";
+
+            // Show the dialog and get the result
+            DialogResult result = openFileDialog.ShowDialog();
+
+            // Check if the user clicked OK
+            if (result == DialogResult.OK)
+            {
+                // Get the selected file names (for multiple files)
+                string[] selectedFiles = openFileDialog.FileNames;
+
+                // Display the selected files
+                Console.WriteLine("Selected Files:");
+                foreach (string file in selectedFiles)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+            else
+            {
+                //Console.WriteLine("File selection canceled.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MoveItem(1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Pack();
+        }
+        static List<Difference> FindDifferences(byte[] array1, byte[] array2)
+        {
+            List<Difference> differences = new List<Difference>();
+
+            // Check if the arrays are of the same length
+            if (array1.Length != array2.Length)
+            {
+                //Console.WriteLine("Arrays have different lengths.");
+                return differences;
+            }
+
+            // Iterate through each byte and compare
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    differences.Add(new Difference { Index = i, Value1 = array1[i], Value2 = array2[i] });
+                }
+            }
+
+            return differences;
+        }
+
+        // Class to represent a difference
+        class Difference
+        {
+            public int Index { get; set; }
+            public byte Value1 { get; set; }
+            public byte Value2 { get; set; }
+        }
+    }
+}
