b495df68289e12381b7786a931bc604ba08de2de
Ref <ref-xx@users.noreply.github.com>
2026-04-28 04:58:22 +0300

    V2 with image previews.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,18 +33,26 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnGenerateTap = new System.Windows.Forms.Button();
+            this.ComboBorder = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLoopCount = new System.Windows.Forms.TextBox();
+            this.chkCompressFirstFrame = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 37);
             this.listBox1.Name = "listBox1";
@@ -85,17 +93,31 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.listBox2);
-            this.groupBox1.Location = new System.Drawing.Point(368, 37);
+            this.groupBox1.Location = new System.Drawing.Point(368, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(331, 368);
+            this.groupBox1.Size = new System.Drawing.Size(331, 316);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(319, 277);
+            this.listBox2.TabIndex = 1;
+            // 
             // button4
             // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button4.Location = new System.Drawing.Point(624, 407);
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(506, 412);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 5;
@@ -103,20 +125,10 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // listBox2
-            // 
-            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(6, 19);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(319, 316);
-            this.listBox2.TabIndex = 1;
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(577, 409);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(665, 377);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(28, 20);
             this.textBox1.TabIndex = 2;
@@ -124,18 +136,104 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(365, 412);
+            this.label1.Location = new System.Drawing.Point(453, 380);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(206, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "Delay, 0=nodelay, 50=1second, max=255:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(392, 352);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Space key to exit";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // btnGenerateTap
+            // 
+            this.btnGenerateTap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateTap.Location = new System.Drawing.Point(587, 412);
+            this.btnGenerateTap.Name = "btnGenerateTap";
+            this.btnGenerateTap.Size = new System.Drawing.Size(112, 23);
+            this.btnGenerateTap.TabIndex = 8;
+            this.btnGenerateTap.Text = "Generate Tap File";
+            this.btnGenerateTap.UseVisualStyleBackColor = true;
+            // 
+            // ComboBorder
+            // 
+            this.ComboBorder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ComboBorder.FormattingEnabled = true;
+            this.ComboBorder.Items.AddRange(new object[] {
+            "Black",
+            "Blue",
+            "Red",
+            "Magenta",
+            "Green",
+            "Cyan",
+            "Yellow",
+            "White"});
+            this.ComboBorder.Location = new System.Drawing.Point(416, 412);
+            this.ComboBorder.Name = "ComboBorder";
+            this.ComboBorder.Size = new System.Drawing.Size(84, 21);
+            this.ComboBorder.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(518, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Loop Count, 0 to Endless:";
+            // 
+            // txtLoopCount
+            // 
+            this.txtLoopCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLoopCount.Location = new System.Drawing.Point(665, 352);
+            this.txtLoopCount.Name = "txtLoopCount";
+            this.txtLoopCount.Size = new System.Drawing.Size(28, 20);
+            this.txtLoopCount.TabIndex = 10;
+            this.txtLoopCount.Text = "0";
+            // 
+            // chkCompressFirstFrame
+            // 
+            this.chkCompressFirstFrame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkCompressFirstFrame.AutoSize = true;
+            this.chkCompressFirstFrame.Location = new System.Drawing.Point(573, 334);
+            this.chkCompressFirstFrame.Name = "chkCompressFirstFrame";
+            this.chkCompressFirstFrame.Size = new System.Drawing.Size(120, 17);
+            this.chkCompressFirstFrame.TabIndex = 11;
+            this.chkCompressFirstFrame.Text = "Compress first frame";
+            this.chkCompressFirstFrame.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 192);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(711, 442);
+            this.Controls.Add(this.chkCompressFirstFrame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLoopCount);
+            this.Controls.Add(this.ComboBorder);
+            this.Controls.Add(this.btnGenerateTap);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
@@ -147,6 +245,7 @@
             this.Name = "Form1";
             this.Text = "ZX Animation Packer 128 by Arda Erdikmen (ref) for helpcomputer0 2023";
             this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,6 +262,13 @@
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnGenerateTap;
+        private System.Windows.Forms.ComboBox ComboBorder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLoopCount;
+        private System.Windows.Forms.CheckBox chkCompressFirstFrame;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,40 @@ namespace ZXAnimPacker128
 {
     public partial class Form1 : Form
     {
+        const int ScreenSize = 6912;
+        const int ScreenBase = 16384;
+        const byte CommandRawRun = 0xDB;
+        const byte CommandCountedLoop = 0xDC;
+        const byte CommandEndlessLoop = 0xDD;
+        const byte CommandEndFrame = 0xDE;
+        const byte CommandRleKeyFrame = 0xDF;
+        const byte CommandSetBorder = 0xF0;
+        const byte CommandRawKeyFrame = 0xFF;
+        static readonly Color[] SpectrumColors = new Color[]
+        {
+            Color.FromArgb(0, 0, 0),
+            Color.FromArgb(0, 0, 205),
+            Color.FromArgb(205, 0, 0),
+            Color.FromArgb(205, 0, 205),
+            Color.FromArgb(0, 205, 0),
+            Color.FromArgb(0, 205, 205),
+            Color.FromArgb(205, 205, 0),
+            Color.FromArgb(205, 205, 205),
+            Color.FromArgb(0, 0, 0),
+            Color.FromArgb(0, 0, 255),
+            Color.FromArgb(255, 0, 0),
+            Color.FromArgb(255, 0, 255),
+            Color.FromArgb(0, 255, 0),
+            Color.FromArgb(0, 255, 255),
+            Color.FromArgb(255, 255, 0),
+            Color.FromArgb(255, 255, 255)
+        };
+
         public Form1()
         {
             InitializeComponent();
+            listBox1.SelectedIndexChanged += new EventHandler(listBox1_SelectedIndexChanged);
+            pictureBox1.Click += new EventHandler(pictureBox1_Click);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -75,6 +106,272 @@ namespace ZXAnimPacker128
             listBox2.Items.Add("File Saved:" + fileName + ".zxa");
         }
 
+        private void PackV2()
+        {
+            byte delay = Convert.ToByte(textBox1.Text);
+            byte keyFrameDelay = delay > 2 ? (byte)(delay - 2) : (byte)0;
+            byte loopCount = Convert.ToByte(txtLoopCount.Text);
+            int borderIndex = ComboBorder.SelectedIndex;
+            if (borderIndex < 0) borderIndex = 0;
+            if (borderIndex > 7) borderIndex = 7;
+
+            if (listBox1.Items.Count == 0)
+            {
+                listBox2.Items.Add("No files selected.");
+                return;
+            }
+
+            List<byte[]> frames = new List<byte[]>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                frames.Add(ReadBytesFromFile(listBox1.Items[i].ToString(), ScreenSize));
+            }
+
+            bool compressFirstFrame = chkCompressFirstFrame.Checked;
+            List<byte> packBytes = new List<byte>(ScreenSize + (frames.Count * 256));
+            packBytes.Add((byte)(CommandSetBorder + borderIndex));
+            listBox2.Items.Add("V2 Border:" + borderIndex.ToString());
+            AddKeyFrameV2(packBytes, frames[0], compressFirstFrame);
+            packBytes.Add(CommandEndFrame);
+            packBytes.Add(keyFrameDelay);
+            listBox2.Items.Add("V2 1.Frame, KeyBytes:" + (packBytes.Count - 1).ToString());
+
+            int offset = packBytes.Count;
+            for (int idx = 0; idx < frames.Count - 1; idx++)
+            {
+                List<byte> deltaBytes = new List<byte>();
+                List<Difference> differences = FindDifferences(frames[idx], frames[idx + 1]);
+                EncodeDifferencesV2(deltaBytes, differences);
+                List<byte> keyFrameBytes = CreateKeyFrameV2(frames[idx + 1], compressFirstFrame);
+
+                if (keyFrameBytes.Count < deltaBytes.Count)
+                {
+                    packBytes.AddRange(keyFrameBytes);
+                    packBytes.Add(CommandEndFrame);
+                    packBytes.Add(keyFrameDelay);
+                    listBox2.Items.Add("V2 " + (2 + idx).ToString() + ".Frame, KeyFrameBytes:" + keyFrameBytes.Count.ToString());
+                }
+                else
+                {
+                    packBytes.AddRange(deltaBytes);
+                    packBytes.Add(CommandEndFrame);
+                    packBytes.Add(delay);
+                }
+
+                listBox2.Items.Add("V2 " + (2 + idx).ToString() + ".Frame, NewBytes:" + (packBytes.Count - offset).ToString());
+                offset = packBytes.Count;
+            }
+
+            if (loopCount == 0)
+            {
+                packBytes.Add(CommandEndlessLoop);
+                listBox2.Items.Add("V2 Loop: Endless");
+            }
+            else
+            {
+                packBytes.Add(CommandCountedLoop);
+                packBytes.Add(loopCount);
+                listBox2.Items.Add("V2 Loop Count:" + loopCount.ToString());
+            }
+
+            byte[] packArray = packBytes.ToArray();
+            listBox2.Items.Add("V2 Total Bytes:" + packArray.Length.ToString());
+            if (packArray.Length < 32768) listBox2.Items.Add("Great! it fits in two banks! Still 48K!");
+            string fileName = Path.GetFileNameWithoutExtension(listBox1.Items[0].ToString());
+            File.WriteAllBytes(fileName + ".zxa", packArray);
+            listBox2.Items.Add("File Saved:" + fileName + ".zxa");
+        }
+
+        private static List<byte> CreateKeyFrameV2(byte[] frame, bool compress)
+        {
+            List<byte> output = new List<byte>(frame.Length + 1);
+            AddKeyFrameV2(output, frame, compress);
+            return output;
+        }
+
+        private static void AddKeyFrameV2(List<byte> output, byte[] frame, bool compress)
+        {
+            if (compress)
+            {
+                AddRleKeyFrameV2(output, frame);
+            }
+            else
+            {
+                AddRawKeyFrameV2(output, frame);
+            }
+        }
+
+        private static void AddRawKeyFrameV2(List<byte> output, byte[] frame)
+        {
+            output.Add(CommandRawKeyFrame);
+            output.AddRange(frame);
+        }
+
+        private static void AddRleKeyFrameV2(List<byte> output, byte[] frame)
+        {
+            output.Add(CommandRleKeyFrame);
+
+            int index = 0;
+            while (index < frame.Length)
+            {
+                int repeatLength = GetRepeatLength(frame, index, 128);
+                if (repeatLength >= 3)
+                {
+                    output.Add((byte)(0x80 + repeatLength - 1));
+                    output.Add(frame[index]);
+                    index += repeatLength;
+                    continue;
+                }
+
+                int literalStart = index;
+                int literalLength = 0;
+                while ((index < frame.Length) && (literalLength < 128))
+                {
+                    repeatLength = GetRepeatLength(frame, index, 128);
+                    if ((repeatLength >= 3) && (literalLength > 0))
+                    {
+                        break;
+                    }
+
+                    if (repeatLength >= 3)
+                    {
+                        break;
+                    }
+
+                    index++;
+                    literalLength++;
+                }
+
+                output.Add((byte)(literalLength - 1));
+                for (int i = 0; i < literalLength; i++)
+                {
+                    output.Add(frame[literalStart + i]);
+                }
+            }
+        }
+
+        private static int GetRepeatLength(byte[] bytes, int start, int maxLength)
+        {
+            int length = 1;
+            while ((start + length < bytes.Length) &&
+                   (length < maxLength) &&
+                   (bytes[start + length] == bytes[start]))
+            {
+                length++;
+            }
+
+            return length;
+        }
+
+        private static void EncodeDifferencesV2(List<byte> output, List<Difference> differences)
+        {
+            bool[] used = new bool[differences.Count];
+
+            for (int i = 0; i < differences.Count; )
+            {
+                int runLength = 1;
+                while ((i + runLength < differences.Count) &&
+                       (differences[i + runLength].Index == differences[i + runLength - 1].Index + 1))
+                {
+                    runLength++;
+                }
+
+                if (runLength >= 3)
+                {
+                    AddRawRunsV2(output, differences, used, i, runLength);
+                }
+
+                i += runLength;
+            }
+
+            List<Difference>[] pageBuckets = new List<Difference>[0x5B];
+            for (int i = 0; i < differences.Count; i++)
+            {
+                if (used[i]) continue;
+
+                int address = differences[i].Index + ScreenBase;
+                int hi = address >> 8;
+                if ((hi < 0x40) || (hi > 0x5A)) continue;
+
+                if (pageBuckets[hi] == null) pageBuckets[hi] = new List<Difference>();
+                pageBuckets[hi].Add(differences[i]);
+            }
+
+            for (int hi = 0x40; hi <= 0x5A; hi++)
+            {
+                if (pageBuckets[hi] == null) continue;
+                AddSamePageOrNormalV2(output, hi, pageBuckets[hi]);
+            }
+        }
+
+        private static void AddRawRunsV2(List<byte> output, List<Difference> differences, bool[] used, int startIndex, int runLength)
+        {
+            int current = startIndex;
+            int remaining = runLength;
+
+            while (remaining >= 3)
+            {
+                int chunkLength = Math.Min(255, remaining);
+                AddRawRunV2(output, differences, current, chunkLength);
+
+                for (int i = 0; i < chunkLength; i++)
+                {
+                    used[current + i] = true;
+                }
+
+                current += chunkLength;
+                remaining -= chunkLength;
+            }
+        }
+
+        private static void AddRawRunV2(List<byte> output, List<Difference> differences, int startIndex, int length)
+        {
+            int address = differences[startIndex].Index + ScreenBase;
+            output.Add(CommandRawRun);
+            output.Add((byte)(address >> 8));
+            output.Add((byte)(address & 0xFF));
+            output.Add((byte)length);
+
+            for (int i = 0; i < length; i++)
+            {
+                output.Add(differences[startIndex + i].Value2);
+            }
+        }
+
+        private static void AddSamePageOrNormalV2(List<byte> output, int hi, List<Difference> pageDifferences)
+        {
+            int index = 0;
+            while (pageDifferences.Count - index >= 3)
+            {
+                int chunkLength = Math.Min(255, pageDifferences.Count - index);
+                output.Add((byte)(hi + 128));
+                output.Add((byte)chunkLength);
+
+                for (int i = 0; i < chunkLength; i++)
+                {
+                    Difference difference = pageDifferences[index + i];
+                    output.Add((byte)((difference.Index + ScreenBase) & 0xFF));
+                    output.Add(difference.Value2);
+                }
+
+                index += chunkLength;
+            }
+
+            while (index < pageDifferences.Count)
+            {
+                AddNormalWriteV2(output, pageDifferences[index]);
+                index++;
+            }
+        }
+
+        private static void AddNormalWriteV2(List<byte> output, Difference difference)
+        {
+            int address = difference.Index + ScreenBase;
+            output.Add((byte)(address >> 8));
+            output.Add((byte)(address & 0xFF));
+            output.Add(difference.Value2);
+        }
+
         static byte[] AddBytes(byte[] originalArray, params byte[] bytesToAdd)
         {
             // Create a new array with enough space for the existing and new bytes
@@ -115,6 +412,39 @@ namespace ZXAnimPacker128
             }
 
             return byteArray;
+        }
+
+        private void ViewSCR(string fileName)
+        {
+            byte[] screen = ReadBytesFromFile(fileName, ScreenSize);
+            Bitmap bitmap = new Bitmap(256, 192);
+
+            for (int y = 0; y < 192; y++)
+            {
+                int bitmapRowOffset = ((y & 0xC0) << 5) | ((y & 0x07) << 8) | ((y & 0x38) << 2);
+                int attributeRowOffset = 6144 + ((y >> 3) * 32);
+
+                for (int xByte = 0; xByte < 32; xByte++)
+                {
+                    byte pixels = screen[bitmapRowOffset + xByte];
+                    byte attribute = screen[attributeRowOffset + xByte];
+                    bool bright = (attribute & 0x40) != 0;
+                    Color ink = SpectrumColors[(attribute & 0x07) + (bright ? 8 : 0)];
+                    Color paper = SpectrumColors[((attribute >> 3) & 0x07) + (bright ? 8 : 0)];
+
+                    for (int bit = 0; bit < 8; bit++)
+                    {
+                        bool pixelSet = (pixels & (0x80 >> bit)) != 0;
+                        bitmap.SetPixel((xByte * 8) + bit, y, pixelSet ? ink : paper);
+                    }
+                }
+            }
+
+            Image oldImage = pictureBox1.Image;
+            pictureBox1.Image = bitmap;
+            if (oldImage != null) oldImage.Dispose();
+            pictureBox1.Visible = true;
+            pictureBox1.BringToFront();
         }
 
         private void pickfiles()
@@ -167,8 +497,24 @@ namespace ZXAnimPacker128
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Pack();
+            PackV2();
+
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null) return;
+            ViewSCR(listBox1.SelectedItem.ToString());
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Image oldImage = pictureBox1.Image;
+            pictureBox1.Image = null;
+            if (oldImage != null) oldImage.Dispose();
+            pictureBox1.Visible = false;
+        }
+
         static List<Difference> FindDifferences(byte[] array1, byte[] array2)
         {
             List<Difference> differences = new List<Difference>();
